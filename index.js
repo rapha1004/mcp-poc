8fff395b378828bc5c85217af51a2e5312e8109b
rapha1005 <106275964+rapha1004@users.noreply.github.com>
2026-03-03 21:01:23 +0100

    feat: joke tool

--- index.js
+++ index.js
@@ -1,33 +1,52 @@
 import {
   McpServer,
-  ResourceTemplate,
 } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from "zod";
 
-// Create an MCP server
 const server = new McpServer({
-  name: "discord-controller",
+  name: "mcp-poc",
   version: "1.0.0",
 });
-// Add an addition tool
+
 server.registerTool(
   "add",
   {
     title: "Addition Tool",
     description: "Add two numbers",
-    inputSchema: { a: z.number(), b: z.number() },
+    inputSchema: z.object({
+      a: z.number(),
+      b: z.number(),
+    }),
   },
   async ({ a, b }) => {
-    console.error("okay");
-    
     return {
       content: [{ type: "text", text: String(a + b) }],
     };
   },
 );
 
+server.registerTool(
+  "joke",
+  {
+    title: "generate a joke",
+    description: "generate a hilarious joke",
+    inputSchema: z.object({
+      amount: z.number(),
+    }),
+  },
+  async ({ amount }) => {
+    const res = await fetch(
+      `https://v2.jokeapi.dev/joke/Any?lang=fr&blacklistFlags=nsfw,religious,political,racist,sexist,explicit&amount=${amount}`
+    );
 
-// Start receiving messages on stdin and sending messages on stdout
+    const data = await res.json();
+
+    return {
+      content: [{ type: "text", text: JSON.stringify(data)}],
+    };
+  },
+);
+
 const transport = new StdioServerTransport();
 await server.connect(transport);
